d628325c1893d83455133eee62488799fcd75c96
abhipshapatro <71815946+abhipshapatro@users.noreply.github.com>
2022-12-04 01:48:01 +0530

    updated

--- src/Components/Main.js
+++ src/Components/Main.js
@@ -1,34 +1,36 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import image from "../assets/wheel.png";
 import { FaTimes } from "react-icons/fa";
 import { HiOutlineMail, HiPhone } from "react-icons/hi";
 import { useNavigate } from "react-router-dom";
 function Main() {
+  const [btn, setBtn] = useState("Try your luck");
+  const navigate = useNavigate();
 
-    const [btn, setBtn] = useState("Try your luck")
-    const navigate = useNavigate()
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setBtn("Loading...");
 
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        setBtn("Loading...");
+    // navigates to next page after 2secs
+    setTimeout(() => {
+      navigate("/spinwheel");
+    }, 1000);
+  };
 
-        // navigates to next page after 2secs
-        setTimeout(() => {
-            navigate("/spinwheel")
-        }, 1000);
-    }
-
-    const style = {
-        color: "#49454F"
-    }
+  const style = {
+    color: "#49454F",
+  };
 
   return (
     <div className="flex flex-col md:flex-row gap-10 md:gap-28 lg:gap-40 ">
-
       {/* left side */}
       <div className="flex items-center justify-center md:shrink-0">
         {/* image */}
-        <img src={image} alt="" className="h-48 w-48 md:h-64 md:w-64 lg:h-80 lg:w-80" />
+        <img
+          src={image}
+          alt=""
+          className="absolute -top-24 mb-10 h-48 w-48 md:top-auto md:absolute md:-left-40 md:h-64 md:w-64 lg:relative lg:h-80 lg:w-80"
+        />
       </div>
 
       {/* right side */}
@@ -44,7 +46,7 @@ function Main() {
           <form onSubmit={handleSubmit}>
             {/* email div */}
             <div className="mb-4 bg-bgWhite rounded-md border-b-2 border-gray-700 flex items-center">
-                {/* mail icon */}
+              {/* mail icon */}
               <span className="px-3">
                 <HiOutlineMail size={28} style={style} />
               </span>
@@ -65,7 +67,7 @@ function Main() {
             </div>
             {/* phone number div */}
             <div className="mb-4 bg-bgWhite rounded-md border-b-2 border-gray-700 flex items-center">
-                {/* phone icon */}
+              {/* phone icon */}
               <span className="px-3">
                 <HiPhone size={28} style={style} />
               </span>
@@ -86,7 +88,7 @@ function Main() {
             </div>
             {/* checkbox div */}
             <div className="mb-4 flex border-2 border-black px-3 py-0.5 md:px-4 md:py-1 rounded-md items-center justify-center">
-                {/* checkbox input */}
+              {/* checkbox input */}
               <input
                 type="checkbox"
                 className="form-check-input rounded w-[40px] h-[40px] accent-black"
